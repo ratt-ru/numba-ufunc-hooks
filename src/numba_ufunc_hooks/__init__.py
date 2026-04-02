651b3833d4b0b14777b163a85f7222ec2bb5df14
Simon Perkins <simon.perkins@gmail.com>
2026-04-02 11:34:09 +0200

    Run pre-commit hooks

--- src/numba_ufunc_hooks/__init__.py
+++ src/numba_ufunc_hooks/__init__.py
@@ -1,5 +1,6 @@
 __version__ = "0.0.0"
 
+
 def _install_extension():
-  """Register the fft overloads"""
-  import numba_ufunc_hooks.fft  #
+    """Register the fft overloads"""
+    import numba_ufunc_hooks.fft  # noqa: F401
